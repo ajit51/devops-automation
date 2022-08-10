174131cf759f41d67279ec5866e8f14b5072a363
ajit51 <ajitk433@gmail.com>
2022-08-10 15:46:40 +0530

    added slf4j and @Postconstruct

--- src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
@@ -1,17 +1,28 @@
 package com.example.devopsintegration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
+@Slf4j
 public class DevopsIntegrationApplication {
 
     @GetMapping
     public String message() {
+        log.info("Welcome to javatechie............");
         return "Welcome to javatechie";
+    }
+
+    @PostConstruct
+    public String test() {
+        log.info("Welcome to javatechie...............");
+        return null;
     }
 
     public static void main(String[] args) {
