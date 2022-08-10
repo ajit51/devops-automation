464e2180da7cde6899c69b21a946a512bcc06a33
ajit51 <ajitk433@gmail.com>
2022-08-10 09:31:19 +0530

    added message endpoint

--- src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
@@ -2,12 +2,20 @@ package com.example.devopsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DevopsIntegrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsIntegrationApplication.class, args);
-	}
+    @GetMapping
+    public String message() {
+        return "Welcome to javatechie";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsIntegrationApplication.class, args);
+    }
 
 }
